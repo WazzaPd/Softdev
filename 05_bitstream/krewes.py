1a1976fe50563d9321e0942240dc472ffb7ec148
WazzaPd <jwu31@stuy.edu>
2022-09-29 13:19:16 -0400

    new program

---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,23 @@
+'''
+DISCO:
+1)
+
+QCC:
+1)
+
+OPS SUMMARY:
+1)
+
+'''
+
+d = {}
+
+def convert(file):
+    krewes_file = open(file, "r")
+    content = krewes_files.read()
+    seperated = file.split('@@@')
+    specify = seperated.split('$$$')
+    count = 0
+    for n in len(specify):
+    
+    
