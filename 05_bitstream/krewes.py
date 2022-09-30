9d265794acd2c7a3a5185bcc1d262282a2446c9b
WazzaPd <jwu31@stuy.edu>
2022-09-30 13:17:36 -0400

    added 06_py-csv

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,11 +1,18 @@
-import random as ran
+# Joseph Wu, Nicole Zhou, Purple, Duck
+# SoftDev
+# K05 -- txt file and getting rid of not needed symbols
+# 2022-09-29
+# time spent: 1.6 hr
+
 '''
 DISCO:
 1) When we set the key to different value, original value gets overwritten with new one
-2) Using .split to split strings into a list
-3) range() to use int in a for loop
+2) Using .split() to split strings into a list
+3) "not in" checks what is not in something
 QCC:
-1) How will be add more values to the keys?
+1) Each key will be the period number
+2) The value of each key will have a nested list: [[devo, ducky], [devo, ducky], ...]
+3) What ways can we check if the key is present in the dictionary?
 OPS SUMMARY:
 1) Read the file using open() and .read()
 2) Use .split() to separate at '@@@'
@@ -14,50 +21,12 @@ OPS SUMMARY:
         (a) If not, add the key and set value [[devo, ducky]]
         (b) Otherwise, use .append() to add [devo, ducky] into the value
 5) Print results
-
 '''
 
+import random as ran
+
 def convert(file):
-    krewes_file = open(file, "r")
-    content = krewes_file.read()
-    seperated = content.split('@@@')
-    Big_gnome = []
-    d = {}
-    for n in range(len(seperated)):
-        gnome = seperated[n].split('$$$')
-        Big_gnome.append(gnome)
-    
-    #find if 0th element is/isnt a key
-    for n in range(len(Big_gnome)):
-        cut = Big_gnome[n]
-        not_there = True
-        for n in list(d):
-            if n == cut[0]:
-                not_there == False
-        
-        #create new key and empty list
-        if not_there:
-            d[cut[0]] = []
-        
-        #add values to existing keys
-        for n in d:
-          if cut[0] == n:
-              d[n].append([cut[1],cut[2]])
-    return d
-
-def select(d):
-    keys = list(d.keys())
-    rand_key = ran.choice(keys)
-    rand_val = ran.choice(d.get(rand_key))
-    rand_val.insert(0,rand_key)
-    return rand_val
-
-def testing():
-    print(select(convert('krewes.txt')))
-    
-testing()
-
-def convert_2(file):
+    # reading file
     krewes_file = open(file, "r")
     content = krewes_file.read()
     g = content.split("@@@")
@@ -68,10 +37,10 @@ def convert_2(file):
         g[i] = g[i].split("$$$")
         i += 1
     
+    # populating dictionary
     d = {}
     index = 0
     while index < len(g):
-        # existing_keys = list[d.keys()]
         x = g[index]
         if x[0] not in d:
             d[x[0]] = [[x[1], x[2]]]
@@ -80,18 +49,14 @@ def convert_2(file):
         index += 1
     
     info = devo(d)
-    
     return info
-#     x = g[0]
-#     return x[0]
 
+# dictionary in this format:
+#   {key:[a, b], ...}
 def devo(diction):
     keys = list(diction.keys())
     rand_key = ran.choice(keys)
     rand_val = ran.choice(diction.get(rand_key))
     return 'period: ' + rand_key + '\ndevo: ' + rand_val[0] + '\nducky: ' + rand_val[1]
 
-print(devo(convert('krewes.txt')))
-
-
-
+print(convert('krewes.txt'))
---
+++ 06_py-csv/numbercruncher.py
@@ -0,0 +1,13 @@
+import random as ran
+'''
+DISCO:
+
+QCC:
+
+HOW THIS SCRIPT WORKS:
+
+'''
+
+def convert(file):
+    occupations_file = open(file, "r")
+    content = occupations_file.read()
