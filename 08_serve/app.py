cf9b0549f1a3fdf59f24269a739c4de74e8312e4
WazzaPd <jwu31@stuy.edu>
2022-10-11 12:45:44 -0400

    updated

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -1,3 +1,43 @@
+'''
+ADJ In The House! :: Joseph Wu, Anna Fang, Diana Akhmedova
+SoftDev
+K08 -- Putting it Together
+2022-10-07
+time spent: 0.5 hours
+
+DISCO:
+- \n does not work in Flask, we have to use <br> instead to create a new line.
+- We have to have a main function, which is the first function underneath @app.route(), that will contain all other necessary funtions.
+- The local variables cannot be underneath @app.route().
+
+QCC:
+- What exactly is __name__? What is __main__?
+- Why can we not have multiple functions returning at the same time?
+- Why is Flask a combination of Python and HTML?
+
+OPS SUMMARY:
+
+    RANDOM OCCUPATION GENERATOR:
+    1.  Read the occupations.csv file and store it as a string
+    2.  Split the string with the delimiter '\n'
+    3.  Remove the insignifcant elements of the list
+    4.  Initialize three dictionaries, one for storing the values and two for testing
+    5.  For the rest of the elements in the list, reverse split them once with the delimiter ','
+    6.  Using the list we got from rsplit, initialize all dictionaries with the key of list[0](occupations)
+    7.  Store list[1] (percentage) as the value of one of the dictionaries (dict1)
+    8.  Initalize the other two dictionaries with one(dict2) with the value of [percentage, 0] and the other(total): 0
+    9:  Take the percentage of dict1 and multiplying it by 10, then store the key percentage*10 times in a list
+    10. Using random.choice, pick a element from the list at random
+
+    RETURNING OUTPUT IN FLASK:
+    1.  We made sure to import random and Flask, as well as place the local variables above @app.route().
+    2.  Then we created a main function called output() that holds and returns our other functions with new lines as formatting. This includes:
+            3.  The roster() function that returns our TNPG and Roster.
+            4.  The random_occupation() funtion that includes our random occupation generator code and returns the random occupation + its percentage of the workforce.
+            5.  The list_occupations() funtion that returns a list of our occupation dictionary's keys.
+    6.  We made sure to include a debug that will refresh anytime we save changes on this file.
+'''
+
 import random
 
 from flask import Flask
@@ -17,7 +57,7 @@ txt.pop(0) # remove first element: "'Job Class': 'Percentage'"
 @app.route("/")       # assign fxn to route
 
 def output():
-    return roster() + "<br><br>" + random_occupation()
+    return roster() + "<br><br>" + random_occupation() + "<br><br>" + str(list_occupations())
 
 def roster():
     ret = "ADJ In The House! -- Joseph Wu, Anna Fang, Diana Akhmedova"
@@ -43,8 +83,12 @@ def random_occupation():
     print(__name__)
     return ret
 
-# def list_occupations():
-#     return list(dict.keys())
+def list_occupations():
+    ret = "LIST OF OCCUPATIONS:"
+    temp = list(dict.keys())
+    for i in temp:
+        ret += "<br>" + str(i)
+    return ret
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
